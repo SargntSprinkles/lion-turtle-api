921983f4b2e07efae81092c04562f5118e503fb5
Brett Leatherman <brett.2.leatherman@t-mobile.com>
2022-04-19 23:06:49 -0400

    sets up gql endpoint resolvers for game data with some starting records

---
+++ server/server.go
@@ -0,0 +1,57 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/graph-gophers/graphql-go"
+	"github.com/graph-gophers/graphql-go/relay"
+
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/SargntSprinkles/lion-turtle-api/config"
+	"github.com/sirupsen/logrus"
+)
+
+var server *http.Server
+
+func Serve() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", ping)
+	mux.HandleFunc("/gql", gql)
+
+	if config.Env() == "dev" || config.Env() == "staging" {
+		mux.Handle("/dev", playground.Handler("playground", "/gql"))
+	}
+
+	port := config.Port()
+	server = &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+	logrus.Infof("listening on %s", port)
+	logrus.Fatal(server.ListenAndServe())
+}
+
+func ping(writer http.ResponseWriter, request *http.Request) {
+	switch request.Method {
+	case "GET":
+		writer.WriteHeader(http.StatusOK)
+		fmt.Fprint(writer, "pong")
+	default:
+		http.NotFound(writer, request)
+	}
+}
+
+func gql(w http.ResponseWriter, r *http.Request) {
+	resolverToolChain := &Resolver{}
+	opts := []graphql.SchemaOpt{}
+
+	schema := graphql.MustParseSchema(resolverToolChain.schema(), resolverToolChain, opts...)
+	gqlHandler := &relay.Handler{Schema: schema}
+	ctx := r.Context()
+	ctx = context.WithValue(ctx, "request", r)
+	ctx = context.WithValue(ctx, "response", w)
+	r = r.WithContext(ctx)
+	gqlHandler.ServeHTTP(w, r)
+}
